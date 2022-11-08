1a4e10abfe6e817b1fd520ec9be35d2171ecaa7d
unknown <maykenm7@gmail.com>
2022-11-08 06:36:02 -0500

    datasource, modelos, repositorios y relaciones de microservicios del producto

---
+++ src/models/index.ts
@@ -0,0 +1,6 @@
+export * from './marca.model';
+export * from './categoria.model';
+export * from './id.model';
+export * from './imagen.model';
+export * from './producto.model';
+export * from './categoria-producto.model';
